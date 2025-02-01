82a4d0e7163ac364c83e824aba0f49f067cf727b
karla <kferreiroc@gmail.com>
2025-02-01 13:41:52 -0500

    added the encoder and configs

--- src/main/java/org/team2168/subsystems/CoralFlywheel.java
+++ src/main/java/org/team2168/subsystems/CoralFlywheel.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralFlywheel extends SubsystemBase {
     private static SparkMax flywheelMotor = new SparkMax(0, MotorType.kBrushless); // placeholder ID
+    private static RelativeEncoder flywheelEncoder = flywheelMotor.getAlternateEncoder();
 
     private final double minuteInHundredMs = 600.0;
     private final double TICKS_PER_REV = 2048;
