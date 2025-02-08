328ef0f5895092c3cd9cf62c39e9e83db8045bef
karla <kferreiroc@gmail.com>
2025-02-08 14:54:12 -0500

    created a class for CAN devices

--- src/main/java/org/team2168/Constants.java
+++ src/main/java/org/team2168/Constants.java
@@ -16,4 +16,9 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class CANDevices {
+    public static final int CORAL_PIVOT = 0;
+    public static final int CORAL_FLYWHEEL = 0;
+  }
 }
