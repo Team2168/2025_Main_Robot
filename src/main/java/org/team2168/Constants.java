fb40d6a9330e63e513bb16f4cd966ac46a572fd9
karla <kferreiroc@gmail.com>
2025-02-13 21:00:44 -0500

    created led can id constant

--- src/main/java/org/team2168/Constants.java
+++ src/main/java/org/team2168/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class CANDevices {
+    public static final int LEDs = 5;
+  }
 }
