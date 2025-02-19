80da915d50b88744d8ead1aa9041f6241b79409e
karlaferr <lemonaid752@gmail.com>
2025-02-18 20:42:46 -0500

    updated gear ratio, pid values, and current limit

--- src/main/java/org/team2168/subsystems/CoralPivot.java
+++ src/main/java/org/team2168/subsystems/CoralPivot.java
@@ -11,7 +11,6 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.EncoderConfig;
-import com.revrobotics.spark.config.MAXMotionConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
@@ -33,20 +32,20 @@ public class CoralPivot extends SubsystemBase {
   private static DigitalInput limitSwitch = new DigitalInput(CANDevices.CORAL_PIVOT_LS);
 
 
-  private final double TICKS_PER_REV = 2048;
-  private static final double GEAR_RATIO = 43.66162388;
-  private final int SMART_CURRENT_LIMIT = 20;
-  private final double MAX_ANGLE = degreesToRot(0.0); //TODO: find angles
+  private final double TICKS_PER_REV = 4096;
+  private static final double GEAR_RATIO = 55.55556;
+  private final int SMART_CURRENT_LIMIT = 50;
+  private final double MAX_ANGLE = degreesToRot(-21.0); //TODO: find angles
   private final double MIN_ANGLE = degreesToRot(0.0); 
   private boolean isInverted = false;
   private IdleMode brake = IdleMode.kBrake;
   private double kMaxOutput = 1.0;
   private double kMinOutput = -1.0;
   private int kV = 917;
-  private double setPoint = degreesToRot(0.0); //TODO: find what setpoint is
-  private double kP = 0.0; // TODO: find out PID values
+  private double setPoint = degreesToRot(0.0);
+  private double kP = 0.0145; // TODO: maybe tune values a little more
   private double kI = 0.0;
-  private double kD = 0.0;
+  private double kD = 0.0099;
 
 
   public enum CORAL_PIVOT_ANGLE {
@@ -76,7 +75,7 @@ public class CoralPivot extends SubsystemBase {
     motorConfigs.closedLoop
       .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
       .pid(kP, kI, kD)
-      .velocityFF(1/kV)
+      //.velocityFF(1/kV)
       .outputRange(kMinOutput, kMaxOutput);
 
     motorConfigs.softLimit
@@ -101,7 +100,7 @@ public class CoralPivot extends SubsystemBase {
   public void setCoralPivotPosition(double degrees) {
     degrees = MathUtil.clamp(degrees, MIN_ANGLE, MAX_ANGLE);
     setPoint = degreesToRot(degrees);
-    if (degrees > 0) { // TODO: find limit
+    if (degrees > 0) {
       if (limitSwitch.get()) {
         pivotMotor.set(0.0);
       }
