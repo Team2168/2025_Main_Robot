789153465bccf7442e161ca87a5033c567a44863
karla <kferreiroc@gmail.com>
2025-03-07 17:48:33 -0500

    changed to method that sets the position from one that sets encoder position

--- src/main/java/org/team2168/commands/IntakePivot/setIntakePivotPosition.java
+++ src/main/java/org/team2168/commands/IntakePivot/setIntakePivotPosition.java
@@ -29,7 +29,7 @@ public class setIntakePivotPosition extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    iPivot.setIntakePivotPosition(degrees);
+    iPivot.setIntakePivotAngle(degrees);
   }
 
   // Called once the command ends or is interrupted.
