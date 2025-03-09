540e6ad1a84ca57efb64c2acc28b5985066b8385
karlaferr <kferreiroc@gmail.com>
2025-03-08 19:10:44 -0500

    made l4 autos and fixed the issue where the flywheel wouldn't run

--- src/main/java/org/team2168/commands/Autos.java
+++ src/main/java/org/team2168/commands/Autos.java
@@ -4,6 +4,7 @@
 
 package org.team2168.commands;
 
+import org.team2168.commands.CoralManipulator.DriveCoralFlywheel;
 import org.team2168.commands.CoralManipulator.DriveFlywheelUntilNoCoral;
 import org.team2168.commands.CoralManipulator.SetCoralPivotAngle;
 import org.team2168.commands.lift.DriveLiftHeights;
@@ -18,6 +19,7 @@ import org.team2168.subsystems.SwerveDrivetrain.Swerve;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
@@ -70,25 +72,40 @@ public final class Autos {
   public Command middleScore() {
     return Commands.sequence(
         swerve.resetPosePathplanner("MiddleLeave"),
-        Commands.parallel(swerve.drivePath("MiddleLeave"), new DriveLiftHeights(lift, LiftHeights.L4.getValue()),
-            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon())))
+        Commands.parallel(swerve.drivePath("MiddleLeave"), new DriveLiftHeights(lift, LiftHeights.BARGE.getValue()),
+            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.BARGE.getPivotPositon())))
         .andThen(new DriveFlywheelUntilNoCoral(flywheel, 0.4));
   }
 
   public Command leftScoreSingle() {
     return Commands.sequence(
         swerve.resetPosePathplanner("TwoCoralLeftAuto1st"),
-        Commands.parallel(swerve.drivePath("TwoCoralLeftAuto1st"), new DriveLiftHeights(lift, LiftHeights.L4.getValue()),
-            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon())))
-        .andThen(new DriveFlywheelUntilNoCoral(flywheel, 0.4));
+        Commands.parallel(swerve.drivePath("TwoCoralLeftAuto1st"), new DriveLiftHeights(lift, LiftHeights.BARGE.getValue()).withTimeout(1.5),
+            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.BARGE.getPivotPositon()).withTimeout(2.0)
+            .andThen(Commands.parallel(new DriveCoralFlywheel(flywheel, 0.5), new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.BARGE.getPivotPositon())))));
   }
 
   public Command rightScoreSingle() {
     return Commands.sequence(
         swerve.resetPosePathplanner("TwoCoralRightAuto1st"),
-        Commands.parallel(swerve.drivePath("TwoCoralRightAuto1st"), new DriveLiftHeights(lift, LiftHeights.L4.getValue()),
-            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon())))
-        .andThen(new DriveFlywheelUntilNoCoral(flywheel, 0.4));
+        Commands.parallel(swerve.drivePath("TwoCoralRightAuto1st"), new DriveLiftHeights(lift, LiftHeights.BARGE.getValue()).withTimeout(1.5),
+            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.BARGE.getPivotPositon()).withTimeout(2.0)
+            .andThen(Commands.parallel(new DriveCoralFlywheel(flywheel, 0.5), new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.BARGE.getPivotPositon())))));
   }
 
+  public Command leftScoreSingleL4() {
+    return Commands.sequence(
+        swerve.resetPosePathplanner("TwoCoralRightAuto1st"),
+        Commands.parallel(swerve.drivePath("TwoCoralRightAuto1st"), new DriveLiftHeights(lift, LiftHeights.L4.getValue()).withTimeout(1.5),
+            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon()).withTimeout(2.0)
+            .andThen(Commands.parallel(new DriveCoralFlywheel(flywheel, 0.5), new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon())))));
+  }
+
+  public Command rightScoreSingleL4() {
+    return Commands.sequence(
+        swerve.resetPosePathplanner("TwoCoralRightAuto1st"),
+        Commands.parallel(swerve.drivePath("TwoCoralRightAuto1st"), new DriveLiftHeights(lift, LiftHeights.L4.getValue()).withTimeout(1.5),
+            new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon()).withTimeout(2.0)
+            .andThen(Commands.parallel(new DriveCoralFlywheel(flywheel, 0.5), new SetCoralPivotAngle(pivot, CORAL_PIVOT_POSITION.L4.getPivotPositon())))));
+  }
 }
